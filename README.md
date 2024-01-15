24940f2a78a48f51917573b50933ab864c1d5049
Amanda Rodrigues <amanda.rodrigues@meisters.solutions>
2024-01-15 11:11:46 -0300

    Moving pieces

---
+++ README.md
@@ -0,0 +1,3 @@
+# Chess System with Java!
+
+* Reference: https://github.com/acenelio/chess-system-java/tree/9dc1a77c6a94715289d97a8bd463dd520d293121
--- src/main/java/application/UI.java
+++ src/main/java/application/UI.java
@@ -1,10 +1,13 @@
 package application;
 
 import chess.ChessPiece;
+import chess.ChessPosition;
 import chess.Color;
 
-public class UI {
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
+public class UI {
 
     // https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
 
@@ -27,6 +30,17 @@ public class UI {
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+    public static ChessPosition readChessPosition(Scanner sc) {
+        try {
+            String s = sc.nextLine();
+            char column = s.charAt(0);
+            int row = Integer.parseInt((s.substring(1)));
+            return new ChessPosition(column, row);
+        } catch (RuntimeException exp) {
+            throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to a8");
+        }
+    }
+
     public static void printBoard(ChessPiece[][] pieces) {
         for (int i = 0; i < pieces.length; i++) {
             System.out.print((8 - i) + " ");
@@ -42,7 +56,7 @@ public class UI {
         if (piece == null) {
             System.out.print("-");
         } else {
-            if (piece.getColor() == Color.WHITE){
+            if (piece.getColor() == Color.WHITE) {
                 System.out.print(ANSI_WHITE + piece + ANSI_RESET);
             } else {
                 System.out.print(ANSI_GREEN + piece + ANSI_RESET);
